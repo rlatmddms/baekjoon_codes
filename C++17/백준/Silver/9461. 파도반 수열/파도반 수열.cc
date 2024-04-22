011bde7c11db8182cf3e457a1dbfa5f7868ae771
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-04-23 05:39:33 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,56 @@
+                        /*<입출력 라이브러리>*/
+#include <iostream>
+//#include <cstdio>
+                        /*<c++ 컨테이너 라이브러리>*/
+#include <vector>
+// #include <queue>
+// #include <deque>
+// #include <stack>
+// #include <list>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <tuple>
+                        /*<기타 라이브러리>*/
+// #include <string>
+// #include <algorithm>
+// #include <random>
+                        /*<기존 c표준 라이브러리>*/
+// #include <cmath>
+// #include <ctime>
+// time_t start = clock();
+// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
+#define ABS(x) ((x<0)?(-x):(x))
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double llf;
+typedef float f;
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+//a<b 오름차순, a>b내림차순
+//true면 변동x, false이면 순서 바뀜
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ull arr[101];
+    arr[1] = 1;
+    arr[2] = 1;
+    arr[3] = 1;
+    for(int i = 4; i <= 100; i++) {
+        arr[i] = arr[i-2] + arr[i-3];
+        // cout << arr[i] << '\n';
+    }
+    int t; cin >> t;
+    for(int i = 0; i < t; i++) {
+        int n; cin >> n;
+        cout << arr[n] << '\n';
+    }
+}
