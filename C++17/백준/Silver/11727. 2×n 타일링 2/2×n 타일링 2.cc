ab5d37c919ecde59cf964bceaf5ed9ed76688b54
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-04-24 11:53:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,50 @@
+                        /*<입출력 라이브러리>*/
+#include <iostream>
+//#include <cstdio>
+                        /*<c++ 컨테이너 라이브러리>*/
+// #include <vector>
+#include <queue>
+// #include <deque>
+// #include <stack>
+// #include <list>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <tuple>
+                        /*<기타 라이브러리>*/
+// #include <string>
+// #include <algorithm>
+// #include <random>
+                        /*<기존 c표준 라이브러리>*/
+// #include <cmath>
+// #include <ctime>
+// time_t start = clock();
+// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
+#define ABS(x) ((x<0)?(-x):(x))
+
+using namespace std;
+typedef unsigned long long ull;
+// typedef long long ll;
+// typedef double llf;
+// typedef float f;
+
+// bool cmp(int a, int b) {
+//     return a > b;
+// }
+//a<b 오름차순, a>b내림차순
+//true면 변동x, false이면 순서 바뀜
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n; cin >> n;
+    queue<int> q;
+    q.push(1); q.push(1);
+    for(int i = 0; i < n-1; i++) {
+        q.push((q.front() * 2 + q.back()) % 10007);
+        q.pop();
+    }
+    cout << q.back();
+}
