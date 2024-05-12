f35014b81a8c347f2485da7d4bcdf0421b3e09b6
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-05-12 23:45:54 +0900

    [Silver II] Title: 이상한 술집, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ C++17/백준/Silver/13702. 이상한 술집/이상한 술집.cc
@@ -0,0 +1,62 @@
+                        /*<입출력 라이브러리>*/
+#include <iostream>
+//#include <cstdio>
+                        /*<c++ 컨테이너 라이브러리>*/
+#include <vector>
+// #include <queue>
+// #include <deque>
+// #include <stack>
+// #include <list>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <tuple>
+                        /*<기타 라이브러리>*/
+// #include <string>
+// #include <algorithm>
+// #include <random>
+                        /*<기존 c표준 라이브러리>*/
+// #include <cmath>
+// #include <ctime>
+// time_t start = clock();
+// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
+#define ABS(x) ((x<0)?(-x):(x))
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double llf;
+typedef float f;
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+//a<b 오름차순, a>b내림차순
+//true면 변동x, false이면 순서 바뀜
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,k; cin >> n >> k;
+    vector<int> wnwjswk;
+    for(int i = 0; i < n; i++) {
+        int tmp; cin >> tmp;
+        wnwjswk.push_back(tmp);
+    }
+    unsigned int left = 0, right = 2147483647;
+    while(left <= right) {
+        int mid = (left+right) / 2, cnt = 0;
+        for(int i : wnwjswk) {
+            cnt += i/mid;
+        }
+        if(cnt >= k) {
+            left = mid+1;
+        }
+        else {
+            right = mid-1;
+        }
+    }
+    cout << right;
+}
