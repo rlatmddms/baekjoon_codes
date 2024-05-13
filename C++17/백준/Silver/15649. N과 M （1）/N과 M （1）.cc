39618e1b93f0d1b138c1bd993d8621873d39451d
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-05-13 21:34:02 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,67 @@
+                        /*<입출력 라이브러리>*/
+#include <iostream>
+//#include <cstdio>
+                        /*<c++ 컨테이너 라이브러리>*/
+#include <vector>
+// #include <queue>
+// #include <deque>
+// #include <stack>
+// #include <list>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <tuple>
+                        /*<기타 라이브러리>*/
+// #include <string>
+// #include <algorithm>
+// #include <random>
+                        /*<기존 c표준 라이브러리>*/
+// #include <cmath>
+// #include <ctime>
+// time_t start = clock();
+// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
+#define ABS(x) ((x<0)?(-x):(x))
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double llf;
+typedef float f;
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+//a<b 오름차순, a>b내림차순
+//true면 변동x, false이면 순서 바뀜
+
+bool visit[9] = {true,true,true,true,true,true,true,true,true};
+int arr[9] = {0,};
+int n,m;
+
+void dfs(int cnt) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        if(visit[i]) {
+            visit[i] = false;
+            arr[cnt] = i;
+            dfs(cnt+1);
+            visit[i] = true;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    dfs(0);
+}
