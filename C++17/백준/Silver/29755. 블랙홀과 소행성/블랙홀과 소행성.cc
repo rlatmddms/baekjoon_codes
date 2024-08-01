272e39b3accd117d7d25211352303cdaecaec9e6
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-08-02 00:36:44 +0900

    [Silver I] Title: 블랙홀과 소행성, Time: 92 ms, Memory: 6252 KB -BaekjoonHub

--- C++17/백준/Silver/29755. 블랙홀과 소행성/블랙홀과 소행성.cc
+++ C++17/백준/Silver/29755. 블랙홀과 소행성/블랙홀과 소행성.cc
@@ -15,6 +15,9 @@ bool cmp2(int a, int b) {
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n,m; cin >> n >> m;
     vector<int> b;
     vector<pair<int,int>> p;
