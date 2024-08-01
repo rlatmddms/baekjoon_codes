dcb7803cc9cff5de1fc8ad91d3836f34734682b1
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-08-01 23:37:51 +0900

    [Silver IV] Title: 사이클 단어, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1544. 사이클 단어/사이클 단어.cc
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int n,cnt = 0; scanf("%d",&n);
+    char words[50][51];
+    int lens[50] = {0}, word = 0;
+    getchar();
+    for(int i = 0; i < n; i++) {
+        char tmp[51]; int tmplen = 0;
+        while(1) {
+            tmp[tmplen++] = getchar();
+            if(tmp[tmplen-1] == '\n' || tmp[tmplen-1] == -1) {
+                tmp[tmplen-1] = '\0';
+                tmplen--;
+                break;
+            }
+        }
+        int put_this_word = 1;
+        for(int j = 0; j < word; j++) {
+            if(lens[j] == tmplen) {
+                for(int l = 0; l < tmplen; l++) {
+                    int is_all_same = 1;
+                    for(int p = 0; p < tmplen; p++) {
+                        if(words[j][p] != tmp[(l+p)%tmplen]) {
+                            is_all_same = 0;
+                            break;
+                        }
+                    }
+                    if(is_all_same) {
+                        put_this_word = 0;
+                        j = 500;
+                        break;
+                    }
+                }
+            }
+        }
+        if(put_this_word) {
+            lens[word] = tmplen;
+            for(int j = 0; j < tmplen; j++) {
+                words[word][j] = tmp[j];
+            }
+            // printf("%d %s %d\n",word, words[word],lens[word]);
+            word++;
+        }
+    }
+    printf("%d",word);
+}
