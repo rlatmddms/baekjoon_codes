6695d05246789e018c5b96bfc6812d166c8e64b5
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2024-08-05 02:35:45 +0900

    [Silver II] Title: 침투, Time: 24 ms, Memory: 17156 KB -BaekjoonHub

--- C++17/백준/Silver/13565. 침투/침투.cc
+++ C++17/백준/Silver/13565. 침투/침투.cc
@@ -1,39 +1,7 @@
-                        /*<입출력 라이브러리>*/
 #include <iostream>
-//#include <cstdio>
-                        /*<c++ 컨테이너 라이브러리>*/
 #include <vector>
-// #include <queue>
-// #include <deque>
-// #include <stack>
-// #include <list>
-// #include <map>
-// #include <set>
-// #include <unordered_map>
-// #include <unordered_set>
-// #include <tuple>
-                        /*<기타 라이브러리>*/
-#include <string>
-// #include <algorithm>
-// #include <random>
-                        /*<기존 c표준 라이브러리>*/
-// #include <cmath>
-// #include <ctime>
-// time_t start = clock();
-// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
-#define ABS(x) ((x<0)?(-x):(x))
 
 using namespace std;
-typedef unsigned long long ull;
-typedef long long ll;
-typedef double llf;
-typedef float f;
-
-bool cmp(int a, int b) {
-    return a > b;
-}
-//a<b 오름차순, a>b내림차순
-//true면 변동x, false이면 순서 바뀜
 
 int n,m;
 vector<string> board; 
