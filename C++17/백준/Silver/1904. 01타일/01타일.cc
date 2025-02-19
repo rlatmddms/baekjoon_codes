c593e925d2e70abbf595cc39f42773319d9e545a
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-02-19 13:01:27 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/1904. 01타일/01타일.cc
+++ C++17/백준/Silver/1904. 01타일/01타일.cc
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    int a = 1, b = 1;
+    int a = 1,b=1;
     for(int i = 0; i < n; i++) {
-        int tmp = a+b;
-        a = b%15746;
-        b = tmp%15746;
+        int tmp = a;
+        a = b;
+        b = (tmp+b)%15746;
     }
-    cout << a%15746;
+    cout << a;
 }
