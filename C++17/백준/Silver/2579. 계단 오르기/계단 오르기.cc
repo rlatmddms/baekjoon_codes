8867c9f3fda11276db3f9985b5563d576978dd61
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-02-19 13:00:58 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ C++17/백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 //#include <cstdio>
                         /*<c++ 컨테이너 라이브러리>*/
-// #include <vector>
+#include <vector>
 #include <queue>
 // #include <deque>
 // #include <stack>
@@ -24,40 +24,41 @@
 #define ABS(x) ((x<0)?(-x):(x))
 
 using namespace std;
-// typedef unsigned long long ull;
-// typedef long long ll;
-// typedef double llf;
-// typedef float f;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double llf;
+typedef float f;
 
-// bool cmp(int a, int b) {
-//     return a > b;
-// }
+bool cmp(int a, int b) {
+    return a > b;
+}
 //a<b 오름차순, a>b내림차순
 //true면 변동x, false이면 순서 바뀜
+
+void solution(int* arr, int n) {
+    queue<pair<int,int>> q;
+    q.push(make_pair(0,0));
+    q.push(make_pair(0,arr[0]));
+    for(int i = 1; i < n; i++) {
+        int big = q.front().first > q.front().second ? q.front().first : q.front().second;
+        q.pop();
+        q.push(make_pair(q.front().second + arr[i],big+arr[i]));
+    }
+    q.pop();
+    int big = q.front().first > q.front().second ? q.front().first : q.front().second;
+    cout << big;
+}
+
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n; cin >> n;
-    if(n == 1) {
-        cin >> n;
-        cout << n;
-        return 0;
+    int arr[500], n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    queue<pair<int,int>> q;
-    int tmp; cin >> tmp;
-    q.push(make_pair(tmp,0));
-    // cout  << q.back().first << ' ' << q.back().second << '\n';
-    cin >> tmp;
-    q.push(make_pair(tmp+q.back().first,tmp));
-    // cout  << q.back().first << ' ' << q.back().second << '\n';
-    for(int i = 0; i < n-2; i++) {
-        cin >> tmp;
-        int max = q.front().second < q.front().first ? q.front().first : q.front().second;
-        q.push(make_pair(tmp + q.back().second, tmp + max));
-        q.pop();
-        // cout  << q.back().first << ' ' << q.back().second << '\n';
-    }
-    cout << (q.back().first < q.back().second ? q.back().second : q.back().first);
+    solution(arr, n);
 }
