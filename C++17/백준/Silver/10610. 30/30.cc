1f9dc93cb1767cfee3e29ecf7cdc2ddc65e2e8df
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-02-20 13:41:58 +0900

    [Silver IV] Title: 30, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- C++17/백준/Silver/10610. 30/30.cc
+++ C++17/백준/Silver/10610. 30/30.cc
@@ -3,7 +3,9 @@
 int main() {
     char tmp;
     int arr[10] = {0};
-    while(scanf("%c",&tmp) != -1) {
+    while(1) {
+        tmp = getchar();
+        if(tmp == -1) break;
         arr[tmp-'0']++;
     }
     if(arr[0] == 0 || (arr[1] + arr[2]*2 + arr[4]*4 + arr[5]*5 + arr[7]*7 + arr[8]*8) % 3 != 0) {
