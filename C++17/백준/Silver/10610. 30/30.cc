11b8f298bfbadb6798521a5dbefb5829fcf7111e
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-02-20 13:40:22 +0900

    [Silver IV] Title: 30, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- C++17/백준/Silver/10610. 30/30.cc
+++ C++17/백준/Silver/10610. 30/30.cc
@@ -1,23 +1,17 @@
-#include<iostream>
-
-using namespace std;
+#include<stdio.h>
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     char tmp;
     int arr[10] = {0};
-    while(cin >> tmp) {
+    while(scanf("%c",&tmp) != -1) {
         arr[tmp-'0']++;
     }
-    int is_multiple3 = arr[1] + arr[2]*2 + arr[4]*4 + arr[5]*5 + arr[7]*7 + arr[8]*8;
-    if(arr[0] == 0 || is_multiple3 % 3 != 0) {
-        cout << "-1";
+    if(arr[0] == 0 || (arr[1] + arr[2]*2 + arr[4]*4 + arr[5]*5 + arr[7]*7 + arr[8]*8) % 3 != 0) {
+        printf("-1");
     }
     else {
         for(int i = 9; i > -1; i--) {
-            while(arr[i]--) cout << i;
+            while(arr[i]--) putchar(i+48);
         }
     }
 }
