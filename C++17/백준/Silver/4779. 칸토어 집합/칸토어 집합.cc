b083d3ee5072c12a6762ce5485254f5df9219698
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-02-20 11:54:46 +0900

    [Silver III] Title: 칸토어 집합, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,67 @@
+                        /*<입출력 라이브러리>*/
+#include <iostream>
+//#include <cstdio>
+                        /*<c++ 컨테이너 라이브러리>*/
+#include <vector>
+#include <queue>
+// #include <deque>
+// #include <stack>
+// #include <list>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <tuple>
+                        /*<기타 라이브러리>*/
+// #include <string>
+// #include <algorithm>
+// #include <random>
+                        /*<기존 c표준 라이브러리>*/
+// #include <cmath>
+// #include <ctime>
+// time_t start = clock();
+// cout << endl << (double)(clock() - start) / CLOCKS_PER_SEC << "sec" << endl;
+#define ABS(x) ((x<0)?(-x):(x))
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double llf;
+typedef float f;
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+//a<b 오름차순, a>b내림차순
+//true면 변동x, false이면 순서 바뀜
+
+void jagyu(int n) {
+    int p3 = 1;
+    for(int i = 0; i < n; i++) {
+        p3 *= 3;
+    }
+    for(int i = 0; i < p3; i++) {
+        cout << ' ';
+    }
+}
+void jagyu_(int n) {
+    if(n == 0) cout << '-';
+    else {
+        jagyu_(n-1);
+        jagyu(n-1);
+        jagyu_(n-1);
+    }
+}
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n; 
+    while(cin >> n) {
+        jagyu_(n);
+        cout << '\n';
+    }
+}
