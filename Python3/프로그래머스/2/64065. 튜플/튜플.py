127278262b9578f1d91083c89de18fd6cc1de79b
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-07 00:19:18 +0900

    [level 2] Title: 튜플, Time: 41.54 ms, Memory: 22.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/64065. 튜플/튜플.py
+++ Python3/프로그래머스/2/64065. 튜플/튜플.py
@@ -1,15 +1,8 @@
 def solution(s):
     answer = []
     s = s[2:-2].split('},{')
-    s = list(map(lambda x : set(list(map(int,x.split(',')))), s))
+    s = list(map(lambda x : set(map(int,x.split(','))), s))
     s.sort(key = lambda x : len(x))
-    answer.append(s[0].pop())
-    s[0].add(answer[-1])
-    # print(s)
-    idx = 1; slen = len(s)
-    while idx < slen:
-        tmp = s[idx] - s[idx-1]
-        answer.append(tmp.pop())
-        # print(tmp.pop())
-        idx+=1
+    for i in s:
+        answer.append((i - set(answer)).pop())
     return answer
