c6244eea3f69fe894baf596976a6bb3bb1f6efe1
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-10 22:46:09 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,10 +1,18 @@
-from itertools import permutations as per
+import sys
+input = sys.stdin.readline
+nm = list(map(int,input().split()))
+lst = []
+visit = [False]*(nm[0]+1)
 
-a,b = map(int,input().split())
-A = []
-for i in range(1,a+1):
-    A.append(str(i))
-for i in per(A,b):
-    for j in i:
-        print(j,end = ' ')
-    print() 
+def dfs(cnt):
+    if cnt == nm[1]:
+        print(*lst)
+        return
+    for i in range(1,nm[0]+1):
+        if not visit[i]:
+            visit[i] = True
+            lst.append(i)
+            dfs(cnt+1)
+            visit[i] = False
+            lst.pop()
+dfs(0)
