1d00a93af582d439e7051b4a919c7ef072cf3f9f
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-10 23:07:51 +0900

    [level 2] Title: N-Queen, Time: 7049.40 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/2/12952. N－Queen/N－Queen.py
+++ Python3/프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,22 +1,23 @@
-def can_put(x,y,lst):
-    for ax,ay in lst:
-        if x == ax or y == ay or x-y == ax-ay or ax+ay == x+y:
-            return False
-    return True
+
 
 def solution(n):
     answer = [0]
     lst = []
-    def dfs(n,cnt):
-        if n == cnt:
+    def can_put(x,y):
+        for ax,ay in lst:
+            if x == ax or y == ay or x-y == ax-ay or ax+ay == x+y:
+                return False
+        return True
+    def dfs(n,x):
+        if n == x:
             # print(lst)
             answer[0] += 1
             return
         for y in range(n):
-            if can_put(cnt,y,lst):
-                lst.append((cnt,y))
-                dfs(n,cnt+1)
+            if can_put(x,y):
+                lst.append((x,y))
+                dfs(n,x+1)
                 lst.pop()
-                
+        
     dfs(n,0)
     return answer[0]
