60f14d546b8c62dd737ed48bd71897d50529f634
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-13 08:44:01 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 14436 KB -BaekjoonHub

--- C++17/백준/Gold/10986. 나머지 합/나머지 합.cc
+++ C++17/백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,4 +1,4 @@
-                        /*<입출력 라이브러리>*/
+/*<입출력 라이브러리>*/
 #include <iostream>
 //#include <cstdio>
                         /*<c++ 컨테이너 라이브러리>*/
@@ -29,61 +29,36 @@ typedef long long ll;
 typedef double llf;
 typedef float f;
 
-bool cmp(int a, int b) {
-    return a > b;
-}
-//a<b 오름차순, a>b내림차순
-//true면 변동x, false이면 순서 바뀜
-
-int biggest4(int a,int b, int c, int d) {
-    a = a > b ? a : b;
-    c = c > d ? c : d;
-    return a > c ? a : c;
-}
-int biggest3(int a,int b, int c) {
-    a = a > b ? a : b;
-    return a > c ? a : c;
-}
-
-int bigger(int a, int c) {
-    return a > c ? a : c;
-}
-
-ll solution(int* arr, int n, int m) {
-    ll sum = 0;
-    arr[0] %= m;
-    // cout << arr[0] << ' ';
-    for(int i = 1; i < n; i++) {
-        arr[i] = (arr[i] + arr[i-1])%m;
-        // cout << arr[i] << ' ';
-    }
-    
-    for(int i = 1; i < m; i++) {
-        int cnt = 0;
-        for(int j = 0; j < n; j++) {
-            if(arr[j] == i) cnt++;
-        }
-        sum += ((ll)cnt * (cnt-1)) / (ll)2;
-    }
-    int cnt = 0;
-    for(int j = 0; j < n; j++) {
-        if(arr[j] == 0) cnt++;
-    }
-    sum += ((ll)cnt * (cnt+1)) / (ll)2;
-
-    return sum;
-}
-
-
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int arr[1000001], n,m;
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+    int n,m; cin >> n >> m;
+    vector<ll> arr;
+    for(int i=0; i<n; i++) {
+        ll a; cin >> a;
+        arr.push_back(a%m);
     }
-    cout << solution(arr, n,m);
+    for(int i=1; i<n; i++) {
+        arr[i] = (arr[i-1] + arr[i])%m;
+    }
+    ll sum = 0,cnt[1000] = {0};
+    for(int i=0; i<n; i++) {
+        if(arr[i] == 0) sum++;
+        else {
+            cnt[arr[i]]++;
+        }
+        // cout << arr[i] << ' ';
+    }
+    // cout << endl;
+    sum = (sum*(sum+1))/(ll)2;
+    for(int i=1; i<m; i++) {
+        if(cnt[i] > 1) {
+            sum += (cnt[i]*(cnt[i]-1))/(ll)2;
+        }
+        // cout << cnt[i] << ' ';
+    }
+    cout << sum;
+    return 0;
+
 }
