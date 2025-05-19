8b101cb3c35adacc204b90e36c24b718abda1574
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-19 17:25:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 113608 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,17 @@
+from collections import deque
+a,b = map(int,input().split())
+visit = [0]*100001
+q = deque([a])
+visit[a] = 1
+while q:
+    v = q.popleft()
+    if v-1 >= 0 and visit[v-1] == 0:
+        visit[v-1] = visit[v] + 1
+        q.append(v-1)
+    if v+1 <= 100000 and visit[v+1] == 0:
+        visit[v+1] = visit[v] + 1
+        q.append(v+1)
+    if v*2 <= 100000 and visit[v*2] == 0:
+        visit[v*2] = visit[v] + 1
+        q.append(v*2)
+print(visit[b]-1)        
