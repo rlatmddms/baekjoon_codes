936a2e0c518f4103226b9d9abe8391b4524ee21d
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-19 17:26:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 113388 KB -BaekjoonHub

--- PyPy3/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ PyPy3/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -5,6 +5,8 @@ q = deque([a])
 visit[a] = 1
 while q:
     v = q.popleft()
+    if v == b:
+        break
     if v-1 >= 0 and visit[v-1] == 0:
         visit[v-1] = visit[v] + 1
         q.append(v-1)
