e700573f3447d77b66ffcc0c82ab0400e886d821
rlatmddms <166594902+rlatmddms@users.noreply.github.com>
2025-05-19 17:05:57 +0900

    [level 2] Title: 큰 수 만들기, Time: 131.35 ms, Memory: 12 MB -BaekjoonHub

--- Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,48 +1,45 @@
+## 풀이 1 : 내 꼴리는대로 만든 코드
+## 오류가 있으나 정답처리 됨...???
 # def solution(number, k):
 #     number = list(number)
 #     nlen = len(number)
+#     number.append('0')
+#     number
 #     idx = 0
 #     answer=''
+#     m = [0]*10
+#     for i in number[idx:idx+k]:
+#         m[ord(i)-48] += 1
 #     rmv = 0
+#     mx = 0
 #     while idx < nlen:
 #         if idx+k-rmv < nlen:
-#             m = max(number[idx:idx+k+1-rmv])
+#             m[ord(number[idx+k-rmv])-48] += 1
+#             for i in range(9,-1,-1):
+#                 if m[i] > 0:
+#                     mx = i
+#                     break
+#             #         print(i,":",m[i],end='||')
+#             # print(mx)
 #         #print(m,number[idx])
-#         if number[idx] < m and rmv < k:
+#         if ord(number[idx])-48 < mx and rmv < k:
+#             m[ord(number[idx+k-rmv])-48] -= 1
 #             rmv+=1
 #         else:
 #             answer += number[idx]
+#         m[ord(number[idx])-48] -= 1
 #         idx+=1
 #     return answer
-
-
 def solution(number, k):
-    number = list(number)
-    
-    nlen = len(number)
-    number+='0'
-    idx = 0
-    answer=''
-    m = [0]*10
-    for i in number[idx:idx+k]:
-        m[ord(i)-48] += 1
     rmv = 0
-    mx = 0
-    while idx < nlen:
-        if idx+k-rmv < nlen:
-            m[ord(number[idx+k-rmv])-48] += 1
-            for i in range(9,-1,-1):
-                if m[i] > 0:
-                    mx = i
-                    break
-            #         print(i,":",m[i],end='||')
-            # print(mx)
-        #print(m,number[idx])
-        if ord(number[idx])-48 < mx and rmv < k:
-            m[ord(number[idx+k-rmv])-48] -= 1
+    number = number[:]
+    stack = []
+    for n in number:
+        while stack and n > stack[-1] and rmv < k :
+            stack.pop()
             rmv+=1
-        else:
-            answer += number[idx]
-        m[ord(number[idx])-48] -= 1
-        idx+=1
-    return answer
+        stack.append(n)
+    while stack and rmv < k:
+        stack.pop()
+        rmv+=1
+    return ''.join(stack)
